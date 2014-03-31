db159b999b8ed2de8951087389e22644daca14d2
kelly-ireland <kelly_ireland_ca@yahoo.com>
2014-03-31 10:16:40 -0700

    Updating copyrights.

--- ConnectionUtils.py
+++ ConnectionUtils.py
@@ -18,7 +18,7 @@
 # * Neither the name of Yahoo. nor the names of its
 # contributors may be used to endorse or promote products
 # derived from this software without specific prior
-# written permission of Yahoo.
+# written permission of Yahoo. 
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
 # IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
